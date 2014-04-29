42309272d1d2985e50efae3e5f62fcda6a8eeeea
mchandleraz <info@mattchandleraz.com>
2014-04-29 10:15:18 -0700

    Adds router and htaccess

---
+++ router.php
@@ -0,0 +1,27 @@
+class Router {
+	private $routes;
+
+	function __construct(){
+		$this->routes = array(
+			"home" => "indexPage",
+			"signup" => "signUp",
+			"login" => "login",
+			"buddies" => "buddies",
+			"ribbit" => "newRibbit",
+			"logout" => "logout",
+			"public" => "publicPage",
+			"profiles" => "profiles",
+			"unfollow" => "unfollow",
+			"follow" => "follow"
+        );
+	}
+
+	public function lookup(){
+		if (array_key_exists($query, $this->routes)) {
+			return $this->routes[$query];
+		} else {
+			return false;
+		}
+	}
+
+}
